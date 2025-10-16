11454af07617328549ea11ab309971b8b443e2da
JusPetBob <JusPetBob@gmail.com>
2025-10-16 22:38:30 +0200

    gui update for pool ordering

--- gui.py
+++ gui.py
@@ -1,10 +1,12 @@
 import ttkbootstrap as ttk
+from ttkbootstrap.scrolled import ScrolledFrame
 from tkinter import filedialog
 import webbrowser
 import os
 from info import check_updates
 from difflib import SequenceMatcher
 from backend import Backend
+
 
 class Error:
     def __init__(self,message,parent:ttk.Window):
@@ -29,10 +31,98 @@ class Error:
         self.parent.focus_get()
         self.parent.deiconify()
 
+class Assignments:
+    def __init__(self, parent:ttk.Frame, backend:Backend):
+        self.parent = parent
+        self.df = backend.df
+        
+        self.columns_norm = backend.cols
+        
+        self.var = []
+        
+        self.elements = []
+        
+        for i,c in enumerate(self.columns_norm):
+            likely = sorted(self.df.columns,key=lambda x:SequenceMatcher(None,x,c).ratio(),reverse=True)[0]
+            
+            self.var.append(ttk.StringVar())
+            self.var[-1].trace_add("write",lambda v,i,m,ind=i:self.update_df(ind))
+            self.var[-1].set(likely)
+            
+            self.elements.append(
+                [
+                    ttk.Label(self.parent,text=c),
+                    ttk.Combobox(self.parent,values=list(self.df.columns),textvariable=self.var[-1])
+                ]
+            )
+            self.elements[-1][0].grid(row=0,column=i,padx=6,pady=(4,2),sticky="w")
+            self.elements[-1][1].grid(row=1,column=i,padx=6,pady=(2,6),sticky="w")
+
+    def update_df(self,i):
+        cols = self.df.columns.values.tolist()
+        self.df.columns.values[cols.index(self.var[i].get())]=self.columns_norm[i]
+
+
+class Content:
+    def __init__(self, parent:ttk.Frame, backend:Backend):
+        self.nb = ttk.Notebook(parent)
+        
+        for i in range(len(backend.pools)):
+            self.nb.add(self.Pool(self.nb,i,backend), text=f"Pool {i+1}",sticky="nw")
+        
+        self.nb.pack(anchor="w",fill=ttk.BOTH)
+    
+    class Pool(ttk.Frame):
+        def __init__(self,master,idx,backend:Backend):
+            super().__init__(master)
+            
+            self.persons = []
+            self.backend = backend
+            
+            for n,row in backend.pools[idx].iterrows():###change to txt vars 
+                self.persons.append(
+                        [
+                        ttk.Entry(self,validate="key",validatecommand=lambda e:self.edit_name(e,n),t),#####################################
+                        ttk.Entry(self,validate="key",validatecommand=lambda e:self.edit_firstname(e,n)),
+                        ttk.Entry(self,validate="key",validatecommand=lambda e:self.edit_age(e,n)),
+                        ttk.Entry(self,validate="key",validatecommand=lambda e:self.edit_weight(e,n)),
+                        ttk.Entry(self,validate="key",validatecommand=lambda e:self.edit_club(e,n)),
+                    ]
+                )
+                
+                for c,p in enumerate(self.persons[-1]):
+                    p.grid(row=n,column=c)
+        
+        def edit_name(self,event,idx):####refractor
+            print(event)
+            #self.backend.df.loc[idx,"Name"] = 
+
+        def edit_firstname(self,event,idx):##
+            print(event)
+            #self.backend.df.loc[idx,"Name"] = 
+
+        def edit_age(self,event,idx):##
+            print(event)
+            #self.backend.df.loc[idx,"Name"] = 
+
+        def edit_weight(self,event,idx):##
+            print(event)
+            #self.backend.df.loc[idx,"Name"] =         
+
+        def edit_club(self,event,idx):##
+            print(event)
+            #self.backend.df.loc[idx,"Name"] = 
+
+        class Person:####### rm this
+            def __init__(self,master,idx,backend:Backend,p_idx):
+                print(idx,backend,p_idx)
+                
+
 
 class GUI:
     root:ttk.Window
     backend:Backend
+    m_cont:Content
     def __init__(self,app):
         self.app = app
         
@@ -55,14 +145,14 @@ class GUI:
         
         #-frames
         self.c_frame = ttk.Frame(self.root)
-        self.c_frame.grid(row=0,column=0,padx=2,pady=2,sticky="w")
+        self.c_frame.pack(padx=2,pady=2,anchor="w")
         
         #assingment frame
         self.a_frame = ttk.Frame(self.root)
-        self.a_frame.grid(row=1,column=0,padx=6,pady=6,sticky="w")
+        self.a_frame.pack(padx=6,pady=6,anchor="w")
         
         self.m_frame = ttk.Frame(self.root)
-        self.m_frame.grid(row=2,column=0,padx=6,pady=6,sticky="w")
+        self.m_frame.pack(padx=6,pady=6,anchor="w",fill=ttk.BOTH)
         
         #-components
         self.g_file_e = ttk.Entry(self.c_frame,width=80)
@@ -139,38 +229,8 @@ class GUI:
     
     def start(self):
         self.backend.run()
+        
+        self.m_cont = Content(self.m_frame,self.backend)
 
     def run(self):
         self.root.mainloop()
-
-
-class Assignments:
-    def __init__(self, parent:ttk.Frame, backend:Backend):
-        self.parent = parent
-        self.df = backend.df
-        
-        self.columns_norm = backend.cols
-        
-        self.var = []
-        
-        self.elements = []
-        
-        for i,c in enumerate(self.columns_norm):
-            likely = sorted(self.df.columns,key=lambda x:SequenceMatcher(None,x,c).ratio(),reverse=True)[0]
-            
-            self.var.append(ttk.StringVar())
-            self.var[-1].trace_add("write",lambda v,i,m,ind=i:self.update_df(ind))
-            self.var[-1].set(likely)
-            
-            self.elements.append(
-                [
-                    ttk.Label(self.parent,text=c),
-                    ttk.Combobox(self.parent,values=list(self.df.columns),textvariable=self.var[-1])
-                ]
-            )
-            self.elements[-1][0].grid(row=0,column=i,padx=6,pady=(4,2),sticky="w")
-            self.elements[-1][1].grid(row=1,column=i,padx=6,pady=(2,6),sticky="w")
-
-    def update_df(self,i):
-        cols = self.df.columns.values.tolist()
-        self.df.columns.values[cols.index(self.var[i].get())]=self.columns_norm[i]
